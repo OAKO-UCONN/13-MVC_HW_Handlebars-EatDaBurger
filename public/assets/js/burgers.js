1c8b92373da625ca72aaef66935a5a7cde48255c
OAKO-UCONN <63368794+OAKO-UCONN@users.noreply.github.com>
2020-10-26 22:26:13 -0400

    ..

--- public/assets/js/burgers.js
+++ public/assets/js/burgers.js
@@ -1,6 +1,6 @@
 // Make sure we wait to attach our handlers until the DOM is fully loaded.
 $(function() {
-  $(".change-sleep").on("click", function(event) {
+  $(".change-devoured").on("click", function(event) {
     var id = $(this).data("id");
     var newDevour = $(this).data("newdevour");
 
