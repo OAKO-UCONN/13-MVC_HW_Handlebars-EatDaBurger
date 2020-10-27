cf45e635a3c5fc286b70e92638fa293950377e87
OAKO-UCONN <63368794+OAKO-UCONN@users.noreply.github.com>
2020-10-26 23:29:39 -0400

    Fixing_DB's

--- db/schema.sql
+++ db/schema.sql
@@ -1,11 +1,11 @@
-CREATE DATABASE IF NOT EXISTS burgers_db;
-USE burgers_db;
+CREATE DATABASE IF NOT EXISTS burger_db;
+USE burger_db;
 
 -- If the table already exists, remove it before trying to create the table again
 DROP TABLE IF EXISTS burger_db;
 
 -- Create the burgers table
-CREATE TABLE burger_db (
+CREATE TABLE burgers (
     id int NOT NULL AUTO_INCREMENT,
     burger_name varchar(255) NOT NULL,
     devoured BOOL DEFAULT false,
