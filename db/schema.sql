c7ce0895a02422242f557194b3478909b5f865f4
OAKO-UCONN <63368794+OAKO-UCONN@users.noreply.github.com>
2020-10-26 22:33:08 -0400

    SQL File updates

--- db/schema.sql
+++ db/schema.sql
@@ -1,3 +1,18 @@
+CREATE DATABASE IF NOT EXISTS burgers_db;
+USE burgers_db;
+
+-- If the table already exists, remove it before trying to create the table again
+DROP TABLE IF EXISTS burgers;
+
+-- Create the burgers table
+CREATE TABLE burgers (
+    id int NOT NULL AUTO_INCREMENT,
+    burger_name varchar(255) NOT NULL,
+    devoured BOOL DEFAULT false,
+    PRIMARY KEY (id)
+);
+
+/*
 ### Schema
 
 CREATE DATABASE burgers_db;
@@ -10,3 +25,4 @@ CREATE TABLE burgers
 	BOOLEAN DEFAULT false,
 	PRIMARY KEY (id)
 );
+*/
