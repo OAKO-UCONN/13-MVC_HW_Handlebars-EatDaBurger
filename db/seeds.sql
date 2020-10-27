adbf18367f70173aed4c84f447a60a9cfb89fea9
OAKO-UCONN <63368794+OAKO-UCONN@users.noreply.github.com>
2020-10-26 22:32:22 -0400

    sql FILE UPDATES

--- db/seeds.sql
+++ db/seeds.sql
@@ -1,6 +1,14 @@
+----
+INSERT INTO burgers (burger_name, devoured) VALUES ('Cheese Burger', FALSE);
+INSERT INTO burgers (burger_name, devoured) VALUES ('Hamburger', FALSE);
+INSERT INTO burgers (burger_name, devoured) VALUES ('Pizza Burger', FALSE);
+----
+
+/*
 INSERT INTO burgers (name) VALUES ('Mongatlas Burger');
 INSERT INTO burgers (name) VALUES ('Blue Burger');
 INSERT INTO burgers (name, devoured) VALUES ('Swiss Burger', true);
 INSERT INTO burgers (name, devoured) VALUES ('Mozerella Burger', true);
 INSERT INTO burgers (name, devoured) VALUES ('Good Burger', true);
 INSERT INTO burgers (name) VALUES ('Bean Burger');
+*/
