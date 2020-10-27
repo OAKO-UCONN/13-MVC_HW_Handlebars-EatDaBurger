b14f42f955d32e56bea41b0f4e4c6f05a1c29235
OAKO-UCONN <63368794+OAKO-UCONN@users.noreply.github.com>
2020-10-26 22:46:23 -0400

    catsburgers

--- models/burger.js
+++ models/burger.js
@@ -28,5 +28,5 @@ var burger = {
   }
 };
 
-// Export the database functions for the controller (catsController.js).
+// Export the database functions for the controller (burgerssController.js).
 module.exports = burger;
