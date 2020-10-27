b6674ed7ea071390b2c8ef6a4a2798bdb4fd92ca
OAKO-UCONN <63368794+OAKO-UCONN@users.noreply.github.com>
2020-10-26 23:27:17 -0400

    dbs

--- db/0schema (1).sql
+++ db/0schema (1).sql
@@ -2,27 +2,12 @@ CREATE DATABASE IF NOT EXISTS burgers_db;
 USE burgers_db;
 
 -- If the table already exists, remove it before trying to create the table again
-DROP TABLE IF EXISTS burger_db;
+DROP TABLE IF EXISTS burgers;
 
 -- Create the burgers table
-CREATE TABLE burger_db (
+CREATE TABLE burgers (
     id int NOT NULL AUTO_INCREMENT,
     burger_name varchar(255) NOT NULL,
     devoured BOOL DEFAULT false,
     PRIMARY KEY (id)
 );
-
-/*
-### Schema
-
-CREATE DATABASE burgers_db;
-USE burgers_db;
-
-CREATE TABLE burgers
-(
-	id int NOT NULL AUTO_INCREMENT,
-	name varchar(255) NOT NULL,
-	BOOLEAN DEFAULT false,
-	PRIMARY KEY (id)
-);
-*/
