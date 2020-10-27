6ca599a9306f35194521cae28135a4863baccd6d
OAKO-UCONN <63368794+OAKO-UCONN@users.noreply.github.com>
2020-10-26 23:24:06 -0400

    devoury

--- public/assets/js/burgers.js
+++ public/assets/js/burgers.js
@@ -1,20 +1,20 @@
 // Make sure we wait to attach our handlers until the DOM is fully loaded.
 $(function() {
-  $(".change-sleep").on("click", function(event) {
+  $(".change-devour").on("click", function(event) {
     var id = $(this).data("id");
-    var newSleep = $(this).data("newsleep");
+    var newdevour = $(this).data("newdevour");
 
-    var newSleepState = {
-      sleepy: newSleep
+    var newdevourState = {
+      devoured: newdevour
     };
 
     // Send the PUT request.
     $.ajax("/api/burgers/" + id, {
       type: "PUT",
-      data: newSleepState
+      data: newdevourState
     }).then(
       function() {
-        console.log("changed sleep to", newSleep);
+        console.log("changed devour to", newdevour);
         // Reload the page to get the updated list
         loburgerion.reload();
       }
@@ -27,7 +27,7 @@ $(function() {
 
     var newburger = {
       name: $("#ca").val().trim(),
-      sleepy: $("[name=sleepy]:checked").val().trim()
+      devoured: $("[name=devoured]:checked").val().trim()
     };
 
     // Send the POST request.
